75423322ad74c47166b4659248280c450e874c0d
sabarivignesh <36516623+sabarivignesh@users.noreply.github.com>
2018-11-07 21:39:43 -0800

    Updated Entrypoint script to add jobs for Drivers table creation

--- start_all_the_things.py
+++ start_all_the_things.py
@@ -35,7 +35,24 @@ def main():
     from etl.iceland_api.api_iceland_earthquakes import ApiIcelandEarthquakes
     iceland_earthquakes_etl = ApiIcelandEarthquakes()
     if not iceland_earthquakes_etl.run(): # This populates the table
-        raise Exception("ETL Job failed: api_iceland_earthquakes")    
+        raise Exception("ETL Job failed: api_iceland_earthquakes")  
+        
+    #Drivers base table - created with date and time as separate columns
+    from data_models.data_model_api_iceland_drivers import DataModelApiIcelandDrivers
+    iceland_drivers_data_model = DataModelApiIcelandDrivers()
+    iceland_drivers_data_model.drop_table() # Start fresh every time
+    iceland_drivers_data_model.create_table() # This creates the table
+
+    from etl.iceland_api.api_iceland_drivers import ApiIcelandDrivers
+    iceland_drivers_etl = ApiIcelandDrivers()
+    if not iceland_drivers_etl.run(): # This populates the table
+        raise Exception("ETL Job failed: api_iceland_drivers")
+
+    #drivers table - created with timestamp column from date and time
+    from data_models.data_model_drivers_custom import DataModelApiIcelandDriversCustom
+    iceland_drivers_custom_data_model = DataModelApiIcelandDriversCustom()
+    iceland_drivers_custom_data_model.drop_drivers()
+    iceland_drivers_custom_data_model.create_drivers()    
         
 if __name__ == "__main__":
     main()
